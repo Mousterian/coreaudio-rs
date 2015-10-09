77ad51da8b7e365e9203a73d7be6ecb890447085
Mousterian <psandison@hotmail.com>
2015-10-09 22:01:10 +1100

    Cleaned up handling of command line args and passing in path to audio file to use rust String as long as possible.

--- examples/playfile.rs
+++ examples/playfile.rs
@@ -7,7 +7,7 @@ use coreaudio::audio_toolbox::audio_file::*;
 use coreaudio::audio_unit::graph::*;
 use coreaudio::error::Error;
 
-fn play_file(filename: &str) -> Result<(),Error> {
+fn play_file(filename: &String) -> Result<(),Error> {
 
     let audio_file_id = try!( open_audio_file(filename) );
     println!("got audio_file_id {:?} ", audio_file_id);
@@ -18,34 +18,28 @@ fn play_file(filename: &str) -> Result<(),Error> {
     // and call it here
 
     // lets set up our playing state now
-    let graph_builder = AUGraph::new();
+    let _graph_builder = AUGraph::new();
 
     Ok(())
 }
 
 fn main() {
-    let mut args = env::args();
-    // ummmm this is a new world of ugliness
-    // there must be a nicer way of doing this
-    args.next();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: playfile <path to audio file>");
+    }
+    else {
+        println!("Using filename: {}", args[1]);
 
-    match args.next() {
-        Some(filename) => {
-            println!("Using filename: {}", filename);
+        let result = play_file(&args[1]);
 
-            let result = play_file(&filename[..]);
-
-            match result {
-                    Ok(_) => {
-                    println!("Everything is ok.");
-                },
-                    Err(err) => {
-                    panic!("Could not play file, error: {:?}", err);
-                }
-                }
-        },
-        None => {
-            println!("Usage: playfile <path to audio file>");
+        match result {
+                Ok(_) => {
+                println!("Everything is ok.");
+            },
+                Err(err) => {
+                panic!("Could not play file, error: {:?}", err);
+            }
         }
     }
 ()
