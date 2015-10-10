3735ef90fa8ad0fc1d068c373be59e4fc87efb06
Mousterian <psandison@hotmail.com>
2015-10-10 22:59:33 +1100

    Fleshed out the AUGraphBuilder with add_node and open methods, and added example code to use them.

--- examples/playfile.rs
+++ examples/playfile.rs
@@ -5,6 +5,7 @@ use std::env;
 
 use coreaudio::audio_toolbox::audio_file::*;
 use coreaudio::audio_unit::graph::*;
+use coreaudio::audio_unit::{Type, SubType, Manufacturer};
 use coreaudio::error::Error;
 
 fn play_file(filename: &String) -> Result<(),Error> {
@@ -12,13 +13,16 @@ fn play_file(filename: &String) -> Result<(),Error> {
     let audio_file_id = try!( open_audio_file(filename) );
     println!("got audio_file_id {:?} ", audio_file_id);
 
-    let _data_format_result = try!( get_data_format(audio_file_id) );
+    let data_format_result = try!( get_data_format(audio_file_id) );
+	println!("got data_format_result {:?} ", data_format_result);
 
     // TO DO: the apple playfile sample has a long complicated stringify method for the asbd, we should port it
     // and call it here
 
-    // lets set up our playing state now
-    let _graph_builder = AUGraph::new();
+    let _au_graph = try!(AUGraph::new()
+						 .add_node(Type::Output, SubType::DefaultOutput, Manufacturer::Apple)
+						 .add_node(Type::Generator, SubType::AudioFilePlayer, Manufacturer::Apple)
+						 .open());
 
     Ok(())
 }
--- src/audio_unit/graph.rs
+++ src/audio_unit/graph.rs
@@ -1,5 +1,6 @@
 use bindings::core_audio as ca;
-use error::{Error, AudioUnitError};
+use super::{Type, SubType, Manufacturer};
+use error::{Error};
 use std::mem;
 
 pub struct AUGraph {
@@ -12,22 +13,18 @@ impl AUGraph {
 	pub fn new() -> AUGraphBuilder {
 		unsafe {
 			let mut graph = mem::uninitialized();
-			let graph_result = match Error::from_os_status(ca::NewAUGraph (&mut graph as *mut ca::AUGraph)) {
+			let result = match Error::from_os_status(ca::NewAUGraph (&mut graph as *mut ca::AUGraph)) {
 				Ok(()) => {
 					println!("Created a new empty AUGraph.");
 					Ok( AUGraph{ graph: graph } )
 				}
 				Err(err) => Err(err)
 			};
-			AUGraphBuilder{ graph_result: graph_result }
+			AUGraphBuilder{ graph_result: result }
 		}
 
 	}
 
-}
-
-pub struct AUGraphBuilder {
-	graph_result: Result<AUGraph, Error>
 }
 
 impl Drop for AUGraph {
@@ -46,6 +43,46 @@ impl Drop for AUGraph {
 			if let Err(err) = error::Error::from_os_status(ca::AUGraphClose(self.graph)) {
 				panic!("{:?}", err.description());
 			}
+		}
+	}
+}
+
+pub struct AUGraphBuilder {
+	graph_result: Result<AUGraph, Error>
+}
+
+impl AUGraphBuilder {
+	pub fn add_node(self, ac_type : Type, ac_sub_type: SubType, ac_manufacturer: Manufacturer) -> AUGraphBuilder {
+		let graph_result = match self.graph_result {
+			Ok(au_graph) => {
+				unsafe {
+					let description = ca::AudioComponentDescription { 	componentType: ac_type as u32,
+																		componentSubType: ac_sub_type as u32,
+																		componentManufacturer: ac_manufacturer as u32,
+																		/* TO DO: figure out does anybody actually use these? */
+																		componentFlags: 0,
+																		componentFlagsMask: 0 };
+					let mut node: ca::AUNode = mem::uninitialized();
+					match Error::from_os_status(ca::AUGraphAddNode(au_graph.graph,
+																	&description as *const ca::AudioComponentDescription,
+																	&mut node as *mut ca::AUNode)) {
+						// store node somewhere ?
+						Ok(()) => Ok(au_graph),
+						Err(e) => Err(e)
+					}
+				}
+			},
+			Err(e) => Err(e)
+		};
+		AUGraphBuilder { graph_result: graph_result }
+	}
+
+	/// Finish building the audio unit graph, and open it.
+	pub fn open(self) -> Result<AUGraph, Error> {
+		let au_graph = try!(self.graph_result);
+		unsafe {
+			try!(Error::from_os_status(ca::AUGraphOpen(&mut *au_graph.graph as ca::AUGraph)));
+			Ok(au_graph)
 		}
 	}
 }
